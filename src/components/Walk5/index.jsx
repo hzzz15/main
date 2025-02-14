7d0dc289f143ae2a41db362c5543bae5665dfffe
Whoru <ssimoorancer@gmail.com>
2025-02-14 15:58:44 +0900

    Walk5 페이지 수정

--- src/components/Walk5/index.jsx
+++ src/components/Walk5/index.jsx
@@ -22,14 +22,14 @@ export default function Walk5() {
       <div className="Walk5-content">
         <div className="Walk5-profile-card">
           <div className="Walk5-profile-image">
-            <img src="/placeholder.svg" alt="반려동물 프로필" width={64} height={64} />
+            <img src="/dogprofile/dog.jpg" alt="반려동물 프로필" width={64} height={64} />
           </div>
           <div className="Walk5-profile-info">
             <div className="Walk5-name">이름</div>
             <div className="Walk5-details">소형견 · 개월수 · 성별</div>
           </div>
           <button className="Walk5-edit-button">
-            <img src="/placeholder.svg" alt="수정" width={24} height={24} />
+            <input type="checkbox" className="Walk5-checkbox" defaultChecked />
           </button>
         </div>
 
