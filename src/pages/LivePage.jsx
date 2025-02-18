3fe0f77763cc1c8ccd318db99d211d3355e16b69
박성빈 <ssimoorancer@gmail.com>
2025-02-18 20:51:10 +0900

    ?

--- src/pages/LivePage.jsx
+++ src/pages/LivePage.jsx
@@ -4,8 +4,8 @@ import BottomNavigation from '../components/BottomNavigation'
 
 function LivePage() {
   return (
-    <div className="page-wrapper">
-      <div className="content-wrapper">
+    <div>
+      <div>
         <Live/>
       </div>
       <BottomNavigation/>
